c73dae28ef22ac7c3c8ae1cb2bdef6faaab84a4d
Nando Rossi <talk@nan.do>
2025-04-04 00:03:32 +0200

    default audio to mute because glitches

--- src/config/gameConfig.js
+++ src/config/gameConfig.js
@@ -2,7 +2,7 @@
 
 // Audio settings
 export const DEFAULT_VOLUME = 30;
-export const DEFAULT_MUTED = false;
+export const DEFAULT_MUTED = true;
 
 // Display settings
 export const DEFAULT_EMOJI_SIZE = 'small'; // small, medium, large
